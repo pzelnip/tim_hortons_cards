63b97aaa30a021e1f4c7817c5aff01c93cb29d59
Adam Parkin <pzelnip@gmail.com>
2026-02-07 14:53:50 -0800

    Add local dev server

---
+++ dev_server.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python3 -m http.server 9214 &
+SERVER_PID=$!
+trap "kill $SERVER_PID" EXIT
+sleep 1
+open http://localhost:9214/2026_olympics.html#VnTU0RV0sulk0VEVwASAC0xM3znJ_-A
+wait $SERVER_PID
